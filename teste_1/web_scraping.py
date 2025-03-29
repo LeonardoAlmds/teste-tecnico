d172ba58e38695b158ddab6e777db12641613e1b
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-03-28 22:32:42 -0300

    function to download completed and tested attachment

--- teste_1/web_scraping.py
+++ teste_1/web_scraping.py
@@ -1,27 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+import wget
 
 class Scraping:
     def __init__(self):
-        # URL para fazer o scraping
         self.page_request = requests.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
         
-        # Faz o parse do conteudo html
         self.page_data = BeautifulSoup(self.page_request.content, "html.parser")
 
         self.get_main_class = self.page_data.find_all("a", class_="internal-link")
 
         self.main()
-    
+
+    def download_file(self, url, file_name):
+        response = requests.get(url, stream=True)
+
+        if response.status_code == 200:
+            with open(file_name, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f"Arquivo {file_name} baixado com sucesso!")
+        else:
+            print(f"Falha ao baixar o arquivo: {url}")
+        
     def get_links(self, class_name):
+        count = 1
         for attribute in class_name:
             href = attribute.get("href")
             
             if href.endswith(".pdf") and "anexo" in href.lower():
-                print(href)
-
+                file_name = f"Anexo_{count}.pdf"
+                self.download_file(href, file_name)
+                count += 1    
+    
     def main(self):
         self.get_links(self.get_main_class)
-
 
 scraping = Scraping()
