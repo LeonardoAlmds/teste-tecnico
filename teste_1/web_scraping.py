7e95633c20a9f7fd96235f7bdc0783e3002a6066
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-03-28 23:14:41 -0300

    function to compress made using the zipfile library as it is more optimized to compress more than one file at a time, test 1 completed.

--- teste_1/web_scraping.py
+++ teste_1/web_scraping.py
@@ -1,39 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
-import wget
+import zipfile
 
 class Scraping:
     def __init__(self):
         self.page_request = requests.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
         
         self.page_data = BeautifulSoup(self.page_request.content, "html.parser")
-
         self.get_main_class = self.page_data.find_all("a", class_="internal-link")
+        self.files = []
 
         self.main()
 
     def download_file(self, url, file_name):
-        response = requests.get(url, stream=True)
+        try:
+            print(f"Baixando {file_name}...")
+            response = requests.get(url, stream=True, timeout=10)
+            response.raise_for_status() 
 
-        if response.status_code == 200:
             with open(file_name, 'wb') as file:
                 for chunk in response.iter_content(chunk_size=8192):
                     file.write(chunk)
             print(f"Arquivo {file_name} baixado com sucesso!")
-        else:
-            print(f"Falha ao baixar o arquivo: {url}")
-        
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao baixar {file_name}: {e}")
+        except IOError as e:
+            print(f"Erro ao salvar o arquivo {file_name}: {e}")
+
     def get_links(self, class_name):
+        print("Iniciando o download dos arquivos...")
         count = 1
+
         for attribute in class_name:
             href = attribute.get("href")
             
-            if href.endswith(".pdf") and "anexo" in href.lower():
+            if href and href.endswith(".pdf") and "anexo" in href.lower():
                 file_name = f"Anexo_{count}.pdf"
+                self.files.append(file_name)
+                
                 self.download_file(href, file_name)
-                count += 1    
-    
+                count += 1   
+
+    def compress_files(self, files):
+        try:
+            print("Compactando arquivos...")
+            zip_filename = "Anexos.zip"
+            
+            with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for file in files:
+                    zipf.write(file)
+            
+            print("Arquivos compactados com sucesso!")
+        except Exception as e:
+            print(f"Erro ao compactar arquivos: {e}")
+
     def main(self):
+        print("Iniciando o scraping...")
         self.get_links(self.get_main_class)
+        
+        if self.files:
+            self.compress_files(self.files)
+        
+        print("Scraping finalizado com sucesso!")
 
 scraping = Scraping()
