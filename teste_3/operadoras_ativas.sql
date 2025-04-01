bb8afe122f0887afd4fdedd8561947faa5436641
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-04-01 15:20:02 -0300

    adding other attributes to tables for control.

--- teste_3/operadoras_ativas.sql
+++ teste_3/operadoras_ativas.sql
@@ -1,24 +1,24 @@
 CREATE TABLE operadoras_ativas (
-    Registro_ANS VARCHAR(6),
-    CNPJ VARCHAR(14),
-    Razao_Social VARCHAR(140),
+    Registro_ANS VARCHAR(6) PRIMARY KEY,
+    CNPJ VARCHAR(14) NOT NULL UNIQUE,
+    Razao_Social VARCHAR(140) NOT NULL,
     Nome_Fantasia VARCHAR(140),
-    Modalidade VARCHAR(2),
-    Logradouro VARCHAR(40),
-    Numero VARCHAR(20),
+    Modalidade VARCHAR(2) NOT NULL,
+    Logradouro VARCHAR(40) NOT NULL,
+    Numero VARCHAR(20) NOT NULL,
     Complemento VARCHAR(40),
-    Bairro VARCHAR(30),
-    Cidade VARCHAR(30),
-    UF CHAR(2),
-    CEP VARCHAR(8),
+    Bairro VARCHAR(30) NOT NULL,
+    Cidade VARCHAR(30) NOT NULL,
+    UF CHAR(2) NOT NULL, 
+    CEP VARCHAR(8) NOT NULL,
     DDD VARCHAR(4),
     Telefone VARCHAR(20),
     Fax VARCHAR(20),
-    Endereco_eletronico VARCHAR(255),
-    Representante VARCHAR(50),
-    Cargo_Representante VARCHAR(40),
+    Endereco_eletronico VARCHAR(255) NOT NULL,
+    Representante VARCHAR(50) NOT NULL,
+    Cargo_Representante VARCHAR(40) NOT NULL,
     Regiao_de_Comercializacao INT,
-    Data_Registro_ANS DATE
+    Data_Registro_ANS DATE NOT NULL
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/Relatorio_cadop.csv' 
